a34abe423f5761595fcddc0ba00da50d4035c300
Matt Juskiw <51274174+mattjuskiw@users.noreply.github.com>
2019-11-10 11:22:57 -0500

    Created /config directory & uploaded connection.js

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "just_do_it"
+});
+
+con.connect(function(err) {
+    if(err) throw err;
+    console.log("Connected");
+});
+
+module.exports = con;
