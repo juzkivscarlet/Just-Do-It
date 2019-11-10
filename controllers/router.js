f08d6f78e3435f3c1852cae9feb21c504d5433dd
Matt Juskiw <51274174+mattjuskiw@users.noreply.github.com>
2019-11-10 11:51:57 -0500

    Created /controllers dir & uploaded router.js

---
+++ controllers/router.js
@@ -0,0 +1,34 @@
+module.exports = (function() {
+    'use strict';
+    var router = require('express').Router();
+    var connection = require('../config/connection.js');
+    var handlebars = require('express-handlebars');
+    var orm = require('../config/orm.js');
+
+    router.get("/", function(req,res) {
+        res.render("index", {
+            song: orm.songs
+        });
+    });
+
+    router.get("/:table", function(req,res) {
+        connection.query(`select * from ??`, [req.params.table], function(err,data) {
+            if(err) throw err;
+            res.send(data);
+        });
+    });
+
+    router.put("/:table/:id", function(req,res) {
+        connection.query(`update ?? set completed = 'yes' where id = ?`, [req.params.table, req.params.id], function(err,result) {
+            if(err) throw err;
+            res.status(200).end();
+            orm.gatherSongs();
+        });
+    });
+
+    router.get("/api/songs", function(req,res) {
+        res.json(orm.songs);
+    });
+
+    return router;
+})();
