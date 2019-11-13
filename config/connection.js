66fb2c94b9f2511a2190b91795e68772e90730e3
Matt Juskiw <51274174+mattjuskiw@users.noreply.github.com>
2019-11-13 14:35:11 -0500

    Reverted connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,7 @@ var mysql = require('mysql');
 var con;
 
 if(process.env.JAWSDB_URL) {
-    con = mysql.createConnection({
-        host: "y06qcehxdtkegbeb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        user: "vfochzi9inggw5wk",
-        password: "yizd22j16l23l4te",
-        port: 3306,
-        database: "pzao8nbq0p6vtcvl"
-    });
+    con = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     con = mysql.createConnection({
         host: "localhost",
