6d286370f1f05b6ac598ef51dff5b9ac42518551
Matt Juskiw <51274174+mattjuskiw@users.noreply.github.com>
2019-11-13 14:26:28 -0500

    Updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "just_do_it"
-});
+var con;
+
+if(process.env.JAWSDB_URL) {
+    con = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    con = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "just_do_it"
+    });
+}
+
 
 con.connect(function(err) {
     if(err) throw err;
