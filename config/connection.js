3cd6873e16720c70a7967c6509c0de58fdaf8523
Matt Juskiw <51274174+mattjuskiw@users.noreply.github.com>
2019-11-14 22:40:40 -0500

    Updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,17 @@
 var mysql = require('mysql');
+require('dotenv').config();
 
 var con;
 
 if(process.env.JAWSDB_URL) {
-    con = mysql.createConnection(process.env.JAWSDB_URL);
+    con = mysql.createConnection({
+        host: process.env.JAWS_HOST,
+        user: process.env.JAWS_USER,
+        password: process.env.JAWS_PASSWORD,
+        port: 3306,
+        database: process.env.JAWS_DATABASE,
+        dialect: "mysql"
+    });
 } else {
     con = mysql.createConnection({
         host: "localhost",
